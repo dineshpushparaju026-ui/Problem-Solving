491ba7b777817337ec2f16789c5c2ba1d9432a7e
Dinesh <dineshpushparaju026@gmail.com>
2026-07-27 18:48:06 +0530

    Changes

--- ex6.py
+++ ex6.py
@@ -10,3 +10,15 @@ for i in range(len(a)):
 
       mul=mul*b[j]
    print(mul)
+
+a=[1,2,3,4]
+
+for i in range(len(a)):
+   mul=1
+
+   for j in range(len(a)):
+
+      if i!=j:
+
+         mul=mul*a[j]
+   print(mul)
