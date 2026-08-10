c476c281fc85952596fb68a86d0189f46055cb77
Dinesh <dineshpushparaju026@gmail.com>
2026-08-10 19:13:47 +0530

    eight commit

---
+++ longestpal.py
@@ -0,0 +1,30 @@
+s = input("Enter a string: ")
+
+longest = ""
+
+for i in range(len(s)):
+
+    left = i
+    right = i
+
+    while left >= 0 and right < len(s) and s[left] == s[right]:
+        if len(s[left:right]) > len(longest):
+            longest = s[left:right]
+
+        left -= 1
+        right += 1
+
+
+
+    left = i
+    right = i + 1
+
+    while left >= 0 and right < len(s) and s[left] == s[right]:
+        if len(s[left:right]) > len(longest):
+            longest = s[left:right+1]
+
+        left -= 1
+        right += 1
+
+
+print(longest)
